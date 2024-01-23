2b97853ea5286c2bb0615698921a508b359c9ba9
Haven McGuire <havenlmcg@gmail.com>
2024-01-23 16:38:05 +0000

    Fixed movement except for on the x-axis only.

--- ASCII Graphics/Scene.cpp
+++ ASCII Graphics/Scene.cpp
@@ -83,95 +83,100 @@ void Scene::move(int index, Coord new_pos) {
 	Coord old_pos = ep->position;
 	Coord change = Coord{ new_pos.x - old_pos.x, new_pos.y - old_pos.y };
 
-	Coord start_pos;
-	Coord end_pos;
-	FrameCode fc;
-
-	Coord spw;
-	Coord epw;
-	Coord spt;
-	Coord ept;
+	Coord start_w;
+	Coord end_w;
+	Coord start_t;
+	Coord end_t;
 
 	// DOWN
 	if (change.y > 0) {
 		// wide rect
-		spw = Coord{ old_pos.x, old_pos.y };
-		epw = Coord{ old_pos.x + width, new_pos.y };
+		start_w = Coord{ old_pos.x, old_pos.y };
+		end_w = Coord{ old_pos.x + width, new_pos.y };
 		
 		// tall rect
-		spt.y = new_pos.y;
-		ept.y = old_pos.y + height;
+		start_t.y = new_pos.y;
+		end_t.y = old_pos.y + height;
 		if (change.x > 0) {
-			spt.x = old_pos.x;
-			ept.x = new_pos.x;
+			start_t.x = old_pos.x;
+			end_t.x = new_pos.x;
 		}
 		else if (change.x < 0) {
-			spt.x = new_pos.x + width;
-			ept.x = old_pos.x + width;
+			start_t.x = new_pos.x + width;
+			end_t.x = old_pos.x + width;
 		}
 	}
 
 	// UP
 	else if (change.y < 0) {
 		// wide rect
-		spw = Coord{ old_pos.x, new_pos.y + height };
-		epw = Coord{ old_pos.x + width, old_pos.y + height };
+		start_w = Coord{ old_pos.x, new_pos.y + height };
+		end_w = Coord{ old_pos.x + width, old_pos.y + height };
 		
 		// tall rect
-		spt.y = old_pos.y;
-		ept.y = old_pos.y + height;
+		start_t.y = old_pos.y;
+		end_t.y = old_pos.y + height;
 		if (change.x > 0) {
-			spt.x = old_pos.x;
-			ept.x = new_pos.x;
+			start_t.x = old_pos.x;
+			end_t.x = new_pos.x;
 		}
 		else if (change.x < 0) {
-			spt.x = new_pos.x + width;
-			ept.x = old_pos.x + width;
+			start_t.x = new_pos.x + width;
+			end_t.x = old_pos.x + width;
 		}
 	}
 	
 	// NONE
-	else if (change.y == 0) {
-		// tall rect
-		for (int row = old_pos.y; row < old_pos.y + height; ++row) {
-			if (change.x > 0) {
-				for (int col = old_pos.x; col < new_pos.x; ++col) {
-					CC::move_cursor_to(col, row);
-					if (index == 0) CC::set_fcolor(0, 0, 0); // default??
-					else CC::set_fcolor(contents[0].entity.frame_data[row][col]);
-					CC::write((char)219);
-				}
-			}
-			else if (change.x < 0) {
-				for (int col = new_pos.x + width; col < old_pos.x + width; ++col) {
-					CC::move_cursor_to(col, row);
-					if (index == 0) CC::set_fcolor(0, 0, 0); // default??
-					else CC::set_fcolor(contents[0].entity.frame_data[row][col]);
-					CC::write((char)219);
-				}
-			}
-		}
-	}
+	//else if (change.y == 0) {
+	//	// tall rect
+	//	for (int row = old_pos.y; row < old_pos.y + height; ++row) {
+	//		if (change.x > 0) {
+	//			for (int col = old_pos.x; col < new_pos.x; ++col) {
+	//				CC::move_cursor_to(col, row);
+	//				if (index == 0) CC::set_fcolor(0, 0, 0); // default??
+	//				else CC::set_fcolor(contents[0].entity.frame_data[row][col]);
+	//				CC::write((char)219);
+	//			}
+	//		}
+	//		else if (change.x < 0) {
+	//			for (int col = new_pos.x + width; col < old_pos.x + width; ++col) {
+	//				CC::move_cursor_to(col, row);
+	//				if (index == 0) CC::set_fcolor(0, 0, 0); // default??
+	//				else CC::set_fcolor(contents[0].entity.frame_data[row][col]);
+	//				CC::write((char)219);
+	//			}
+	//		}
+	//	}
+	//}
 
 
-
-
-	fc = FrameCode{};
-	for (int row = start_pos.y; row < end_pos.y; ++row) {
+	// wide rect
+	FrameCode fcw;
+	for (int row = start_w.y; row < end_w.y; ++row) {
 		std::string line = "";
-		for (int col = start_pos.x; col < end_pos.x; ++col) {
+		for (int col = start_w.x; col < end_w.x; ++col) {
 			if (index == 0) line += to_ansi_fcolor(0, 0, 0); // default??
 			else line += to_ansi_fcolor((contents[0].entity.frame_data[row][col]));
-			//else line += to_ansi_fcolor(Color{ 0,0,0 });
 			line += (char)219;
 		}
-		fc.push_back(line);
+		fcw.push_back(line);
 	}
-	CC::move_cursor_to(start_pos);
-	CC::write(fc);
+	CC::move_cursor_to(start_w);
+	CC::write(fcw);
 
-
-
+	// tall rect
+	FrameCode fct;
+	for (int row = start_t.y; row < end_t.y; ++row) {
+		std::string line = "";
+		for (int col = start_t.x; col < end_t.x; ++col) {
+			if (index == 0) line += to_ansi_fcolor(0, 0, 0); // default??
+			else line += to_ansi_fcolor((contents[0].entity.frame_data[row][col]));
+			line += (char)219;
+		}
+		fct.push_back(line);
+	}
+	CC::move_cursor_to(start_t);
+	CC::write(fct);
 
 	ep->position = new_pos;
 }
